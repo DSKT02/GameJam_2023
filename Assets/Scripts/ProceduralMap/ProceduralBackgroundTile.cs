20dc447ec4c545624f4ec38d3a5f38a58e3e26ca
Diego Rodriguez garcia <diego99gu@hotmail.com>
2023-02-04 16:22:27 -0500

    add variation to background tiles

--- Assets/Scripts/ProceduralMap/ProceduralBackgroundTile.cs
+++ Assets/Scripts/ProceduralMap/ProceduralBackgroundTile.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class ProceduralBackgroundTile : PooledMonoBehaviour
 {
-    
+    [SerializeField] private List<GameObject> options;
+
+    private void OnEnable()
+    {
+        int randomValue = Random.Range(0, options.Count);
+
+        for (int i = 0; i < options.Count; i++)
+        {
+            options[i].SetActive(randomValue == i);
+        }
+    }
 }
