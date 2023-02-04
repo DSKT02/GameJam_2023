bafe630e5d1afad78d527e67962028e72551af71
Felipe Rubio <juan.feliperubiog@gmail.com>
2023-02-04 14:30:50 -0500

    removed rotation test, overall movement improvements

--- Assets/Scripts/Player/PlayerMovement/PlayerMovementInput.cs
+++ Assets/Scripts/Player/PlayerMovement/PlayerMovementInput.cs
@@ -1,15 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovementInput : MonoBehaviour
 {
-    [SerializeField] private float lateralAceleration = 10f;
+    [SerializeField] private float lateralAcceleration = 10f;
     [SerializeField] private float movementRadius = 3f;
-    [SerializeField]
-    [Range(3, 10)] private int rightFallThreshold = 7;
-    [SerializeField]
-    [Range(3, 10)] private int leftFallThreshold = 7;
     [SerializeField] private float forwardVelocity = 5f;
 
     private Transform _playerTransform;
@@ -17,31 +11,14 @@ public class PlayerMovementInput : MonoBehaviour
     private float _screenWidth;
     private float _lateralVelocity = 0f;
     private float _smoothTime = .1f;
-    private float _initalXPos = 0f;
-    private float _rightFallThreshold = 0f;
-    private float _leftFallThreshold = 0f;
-
-    // Test Rotation
-    private float _rotateInterval = 3f;
-    private float _timer;
-    private float _rotationTime = .5f;
-    private float _degreesToRotate = 90f;
-    private float _rotationSpeed;
+    private float _touchTimer;
+    private float _touchInput;
 
     private void Start()
     {
         _playerTransform = GetComponent<Transform>();
         _rootPlayerTransform = _playerTransform.parent.GetComponent<Transform>();
         _screenWidth = Screen.width;
-        _initalXPos = _playerTransform.localPosition.x;
-
-        _rightFallThreshold = (_initalXPos + (movementRadius * (rightFallThreshold / 10f)));
-        _leftFallThreshold = (_initalXPos - (movementRadius * (leftFallThreshold / 10f)));
-
-        _rotationSpeed = _degreesToRotate / _rotationTime;
-
-        print($"Right Threshold: {_rightFallThreshold}");
-        print($"Left Threshold: {_leftFallThreshold}");
 
 #if UNITY_EDITOR
         print($"Touch Support: {Input.touchSupported}");
@@ -52,6 +29,16 @@ public class PlayerMovementInput : MonoBehaviour
     {
         LateralMovement();
         MoveForward();
+
+        UpdatePlayerPosition();
+    }
+
+    private void UpdatePlayerPosition()
+    {
+        float newXPos = Mathf.Clamp(Mathf.SmoothDamp(_playerTransform.localPosition.x, _playerTransform.localPosition.x + _lateralVelocity * Time.deltaTime, ref _lateralVelocity, _smoothTime), -movementRadius, movementRadius);
+        float newYPos = Mathf.Sqrt(Mathf.Pow(movementRadius, 2) - Mathf.Pow(newXPos, 2));
+
+        _playerTransform.localPosition = new Vector3(newXPos, newYPos, _playerTransform.localPosition.z);
     }
 
     private void LateralMovement()
@@ -60,28 +47,30 @@ public class PlayerMovementInput : MonoBehaviour
         {
             if (Input.touchCount > 0)
             {
+                _touchTimer = 0;
                 Vector2 touchPos = Input.GetTouch(0).position;
+                _touchInput = Mathf.Clamp((touchPos.x > _screenWidth / 2) ? _touchInput + Time.deltaTime : _touchInput - Time.deltaTime, -1f, 1f);
 
-                if (touchPos.x > _screenWidth / 2)
-                {
-                    // Move right
-                    _lateralVelocity = lateralAceleration;
-                }
-                else
-                {
-                    // Move left
-                    _lateralVelocity = -lateralAceleration;
-                }
+                _lateralVelocity = _touchInput * lateralAcceleration;
             }
             else
             {
-                StartFalling();
+                // ramp it down to 0
+                if (_touchInput != 0)
+                {
+                    _touchInput = Mathf.Lerp(_touchInput, 0, _touchTimer);
+                    _touchTimer += 3f * Time.deltaTime;
+                    _lateralVelocity = _touchInput * lateralAcceleration;
+                }
+                else
+                {
+                    StartFalling();
+                }
             }
         }
         else
         {
             float input = Input.GetAxis("Horizontal");
-            
 
             if (input == 0)
             {
@@ -89,47 +78,25 @@ public class PlayerMovementInput : MonoBehaviour
             }
             else
             {
-                _lateralVelocity = input * lateralAceleration;
+                _lateralVelocity = input * lateralAcceleration;
             }
         }
 
-        float newXPos = Mathf.Clamp( Mathf.SmoothDamp(_playerTransform.localPosition.x, _playerTransform.localPosition.x + _lateralVelocity * Time.deltaTime, ref _lateralVelocity, _smoothTime), -movementRadius, movementRadius);
-        float newYPos = Mathf.Sqrt(Mathf.Pow(movementRadius, 2) - Mathf.Pow(newXPos, 2));
 
-        _playerTransform.localPosition = new Vector3(newXPos, newYPos, _playerTransform.localPosition.z);
     }
 
     private void MoveForward()
     {
-        _timer += Time.deltaTime;
-
-        if (_timer >= _rotateInterval)
-        {
-            _timer = 0f;
-            //_rootPlayerTransform.rotation *= Quaternion.Euler(0f, 90f, 0f);
-            _rootPlayerTransform.RotateAround(_rootPlayerTransform.position, Vector3.up, _rotationSpeed * Time.deltaTime);
-        }
-
         _rootPlayerTransform.localPosition += _rootPlayerTransform.forward * forwardVelocity * Time.deltaTime;
     }
 
     private void StartFalling()
     {
-        if (_playerTransform.localPosition.x > _rightFallThreshold && _playerTransform.localPosition.x < (_initalXPos + movementRadius))
+        if (_playerTransform.localPosition.x != 0)
         {
-            // Falling right
-            _lateralVelocity += lateralAceleration * Time.deltaTime;
+            _lateralVelocity += (lateralAcceleration * 2.75f) * _playerTransform.localPosition.x * Time.deltaTime;
 #if UNITY_EDITOR
-            print("Falling right");
-#endif
-        }
-
-        if (_playerTransform.localPosition.x < _leftFallThreshold && _playerTransform.localPosition.x > (_initalXPos - movementRadius))
-        {
-            // Falling left
-            _lateralVelocity -= lateralAceleration * Time.deltaTime;
-#if UNITY_EDITOR
-            print("Falling left");
+            print($"Falling {(_playerTransform.localPosition.x > 0 ? "Right" : "Left")}");
 #endif
         }
     }
